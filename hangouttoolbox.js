9c450ae561532844ba46067190a4ad5e66324c52
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2012-10-06 21:51:53 +0200

    Initial Version

--- hangouttoolbox.js
+++ hangouttoolbox.js
@@ -1,17 +1,17 @@
  (function(){
  	/*
 	 * @copyright
-	 * Meme Face for Google+ Hangouts
-	 * Copyright (c) 2012 Moritz Tolxdorff
+	 * Hangout Toolbox for Google+ Hangouts
+	 * Copyright (c) 2012 Moritz Tolxdorff & Martin Thielecke
 	 * 
 	 * Version: 0.0.1
 	 *
-	 * Meme Face for Google+ Hangouts is free software: you can redistribute it and/or modify
+	 * Hangout Toolbox for Google+ Hangouts is free software: you can redistribute it and/or modify
  	 * it under the terms of the GNU General Public License as published by
  	 * the Free Software Foundation, either version 3 of the License, or
  	 * (at your option) any later version.
 	 * 
-	 * Meme Face for Google+ Hangouts is distributed in the hope that it will be useful,
+	 * Hangout Toolbox for Google+ Hangouts is distributed in the hope that it will be useful,
  	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
  	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  	 * GNU General Public License for more details.
@@ -22,20 +22,13 @@
 	*/	
 
 	/**
-	 * @MemeFace
+	 * @HangoutToolbox
 	 * @constructor
 	*/
-	function MemeFace(){
+	function HangoutToolbox(){
 		if(!gapi){
 			throw "gapi not loaded!";
 		}
-
-		/**
-		 * @MemeFace.DEBUGGING - defines if debugging is enabled
-		 * @private
-		 * @type {boolean}
-		*/
-		this.DEBUGGING = false;
 
 		/**
 		 * @MemeFace.maxHeight - defines the maximum window height
@@ -44,28 +37,6 @@
 		 * @type {Number}
 		*/
 		this.maxHeight = jQuery(window).height();
-
-		/**
-		 * @MemeFace.globalShow - defines the initial state of globalShow 
-		 * @private
-		 * @type {boolean}
-		*/
-		this.globalShow = false;
-		this.customShow = false;
-
-		/**
-		 * @MemeFace.overlays - defines the canvasOverlay container 
-		 * @private
-		 * @type {Array}
-		*/
-		this.overlays = {};
-
-		/**
-		 * @MemeFace.canvas - defines the canvas container 
-		 * @private
-		 * @type {null | HTMLCanvasElement}
-		*/
-		this.canvas = null;
 
 		/*
 		 * Bind gapi events when API is ready
@@ -82,7 +53,7 @@
 	 * @buildDOM - Building the DOM structure
 	 * @private
 	*/
-	MemeFace.prototype.buildDOM = function(){
+	HangoutToolbox.prototype.buildDOM = function(){
 		this.log("Building DOM");
 		/*
 		 * Create empty elements
@@ -90,7 +61,6 @@
 		var div = this.createElement("div");
 		var span = this.createElement("span");
 		var cleardiv = this.createElement("div", {"class": "clear"});
-		var inputText = this.createElement("input", {"class": "input", "type": "text"});
 
 		/*
 		 * Create pane header
@@ -110,26 +80,6 @@
 		*/
 		var body = div.clone().attr({"id": "body"}).css({"height": (this.maxHeight-162)+"px"});
 
-		var inputText_url 		= inputText.clone().attr({"id": "URL", "class": "box_text", "url": "url"});
-		var button = this.createElement("button", {"id": "button"}).html("On/Off");
-
-		var grid_container		= div.clone().attr({"class":"grid_container"});
-		var grid_table			= this.createElement("table", {"class":"table_sound"});
-		var content = "";
-		for(var i = 0; i < faces.length; i++){
-			if((i % 3) == 0){
-				content += "<tr>";
-			}
-			content += "<td><a data-playable='" + faces[i].title + "' class='btn-faces' style='background-image: url(https://tolxdorff.appspot.com/a/memeface/i/small/" + faces[i].data + ");' title='" + faces[i].title + "'></a></td>";
-			if((i % 3) == 2){
-				content += "</tr>";
-			}
-		}
-
-		grid_table.append(jQuery(content));
-		grid_container.append(grid_table);
-		jQuery("a[data-playable]").live("click",this.toggleFace.bind(this));
-
 		/*
 		 * Create the footer Div
 		*/
@@ -137,67 +87,12 @@
 		footer.append(this.createElement("a",{"href": "https://google.me/+MoritzTolxdorff", "target": "_blank"}).html("+Moritz Tolxdorff"));
 		footer.append(this.createElement("span", {"class":"version"}).text("v 0.0.1"));
 
-		body.append(shadow, grid_container, inputText_url, button);
+		body.append(shadow);
 
 		/*
 		 * Append DOM structure to container
 		*/
-		jQuery("#container").append(header, body, shadow_bottom, footer);
-
-		button.click(this.LoadURL.bind(this));
-	}
-
-	/**
-	 * @createImageResourceFromCanvas - Creates a image resource from a canvas element
-	 * @private
-	 * @param canvas {HTMLCanvasElement}
-	*/
-	MemeFace.prototype.createFacesResources = function(){
-		for(var i = 0; i < faces.length; i++){
-			this.overlays[faces[i].title] = gapi.hangout.av.effects.createImageResource('https://tolxdorff.appspot.com/a/memeface/i/' + faces[i].data)
-			.createFaceTrackingOverlay(
-				{'trackingFeature': gapi.hangout.av.effects.FaceTrackingFeature.NOSE_ROOT, 
-				 'offset': {"x":0,"y":0},
-				 'rotateWithFace': true, 
-				 'scaleWithFace': true, 
-				 'scale': faces[i].scale
-				});
-		}
-		console.log("Faces generated!");
-	}
-
-	MemeFace.prototype.toggleFace = function(evt){
-		var title = jQuery(evt.target).data("playable");
-		console.log("Clicked Face: ", title);
-		
-		if(this.globalShow === false){
-			this.overlays[title].setVisible(true);	
-			this.globalShow = true;
-		}else{
-			this.overlays[title].setVisible(false);	
-			this.globalShow = false;
-		}
-	}
-
-	MemeFace.prototype.LoadURL = function(){
-		var image_url = $('#URL').val();
-		var image = gapi.hangout.av.effects.createImageResource(image_url);
-		this.overlay = image.createFaceTrackingOverlay({
-      		'trackingFeature': gapi.hangout.av.effects.FaceTrackingFeature.NOSE_ROOT,
-      		'scaleWithFace': true,
-      		'rotateWithFace': true,
-      		'scale': 2.3,
-      		'offset': { 'x': 0, 'y': 0 }
-  		});
-
-  		if(this.customShow === false){
-			this.overlay.setVisible(true);	
-			this.customShow = true;
-		}else{
-			this.overlay.setVisible(false);	
-			this.customShow = false;
-			this.overlay.dispose();
-		}
+		jQuery("#extension").append(header, body, shadow_bottom, footer);
 	}
 
 	/**
@@ -205,7 +100,7 @@
 	 * @private
 	 * @param evt {jQueryEventObject}
 	*/
-	MemeFace.prototype.onWindowResize = function(evt){
+	HangoutToolbox.prototype.onWindowResize = function(evt){
 		this.log("Window resized");
 		this.maxHeight = $(window).height();
 		this.scale();
@@ -215,7 +110,7 @@
 	 * @scale - Scales the body for different resolutions
 	 * @public
 	*/
-	MemeFace.prototype.scale = function(){
+	HangoutToolbox.prototype.scale = function(){
 		/*
 		 * Set the maximum height of the body minus header, input div and footer
 		*/
@@ -227,7 +122,7 @@
 	 * @private
 	 * @param evt {jQueryEventObject}
 	*/
-	MemeFace.prototype.bodyOnScroll = function(evt){
+	HangoutToolbox.prototype.bodyOnScroll = function(evt){
 		/*
 		 * Hide/Show shadow depending on scroll position
 		*/
@@ -240,20 +135,8 @@
 	 * @param type {String} 
 	 * @param attr {Object} 
 	*/
-	MemeFace.prototype.createElement = function(type, attr){
+	HangoutToolbox.prototype.createElement = function(type, attr){
 		return jQuery("<" + type + ">").attr(attr || {});
-	}
-
-	/**
-	 * @log - Writes to console.log if DEBUGGING is enabled
-	 * this.log(...)
-	 * @private
-	 * @param {Mixed}
-	*/
-	MemeFace.prototype.log = function(){
-		if(this.DEBUGGING === true){
-			console.log(Array.prototype.slice.call(arguments))
-		}
 	}
 
 	/**
@@ -261,13 +144,12 @@
 	 * @private
 	 * @param event {gapi.hangout.apiReadyEvent}
 	*/
-	MemeFace.prototype.onApiReady = function(event){
+	HangoutToolbox.prototype.onApiReady = function(event){
 		if(event.isApiReady){
 			try {
-				console.log("Meme Face loaded!");
+				console.log("Hangout Toolbox loaded!");
 				this.buildDOM();
 				this.scale();
-				this.createFacesResources();
 			}	
 			catch(err) {
 				console.log(err);
@@ -276,5 +158,5 @@
 	}
 
 	// Export instantiated LowerThird to main window
-	window["appController"] = new MemeFace();
+	window["appController"] = new HangoutToolbox();
 })()
