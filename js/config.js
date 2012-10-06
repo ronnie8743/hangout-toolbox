aad984ae19e2938591a44c9f9e7791823c92815b
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2012-10-07 01:48:45 +0200

    Dev Update

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var faces = [
+	{title : "LOL", scale: 2.4, data : "lolface.png"},
+	{title : "BOSS", scale: 2.5, data : "boss.png"},
+	{title : "CHALLENGE", scale: 2.5, data : "challenge.png"},
+	{title : "ALONE", scale: 2.8, data : "alone.png"},
+	{title : "MEGUSTA", scale: 2.5, data : "megusta_meme.png"},
+	{title : "PLEASE", scale: 2.8, data : "bitch.png"},
+	{title : "TROLL", scale: 3.0, data : "trollface.png"},
+	{title : "CEREAL", scale: 2.8, data : "cereal.png"},
+	{title : "CUTE", scale: 2.5, data : "cute.png"}
+];
