cfe15abd0546acad9a6270b8a21670bc71db7625
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2012-10-07 22:49:02 +0200

    Update to 1.0.0-RC2

--- hangouttoolbox.js
+++ hangouttoolbox.js
@@ -170,8 +170,6 @@
 					axis: "y",
 					handle: "h3",
 					stop: function( event, ui ) {
-						// IE doesn't register the blur when sorting
-						// so trigger focusout handlers to remove .ui-state-focus
 						ui.item.children( "h3" ).triggerHandler( "focusout" );
 					}
 				});
