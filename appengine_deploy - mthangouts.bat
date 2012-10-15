b7086128fa8892afc6a7632c28f4384330d51171
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2012-10-15 18:46:15 +0200

    cloned new repo

---
+++ appengine_deploy - mthangouts.bat
@@ -0,0 +1,2 @@
+@echo off
+echo  fmqomczxtqgiyqlp | C:\Python27\python.exe "C:\Program Files (x86)\Google\google_appengine\appcfg.py" --email=moritz.tolxdorff@gmail.com --passin update "D:\MTHangouts"
