bb592b5be9c71e892f4799ec9764772c6e6a2910
Martin Thielecke <martin@versionzwei.com>
2012-10-18 18:50:57 +0000

    - removed needless file

--- appengine_deploy - mthangouts.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-echo  fmqomczxtqgiyqlp | C:\Python27\python.exe "C:\Program Files (x86)\Google\google_appengine\appcfg.py" --email=moritz.tolxdorff@gmail.com --passin update "D:\MTHangouts"
