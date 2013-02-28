8f440f7286c30b9fb4b0b4d4b14e0fbf27b10a4c
Martin Thielecke <martin@versionzwei.com>
2013-02-28 20:14:40 +0000

    - re-enabled try-except

--- index.py
+++ index.py
@@ -22,30 +22,30 @@ class ScriptPage(webapp2.RequestHandler):
       'hangouttoolbox.js'
       ]
 
-    #try:
-    response = urllib2.urlopen(url)
+    try:
+      response = urllib2.urlopen(url)
 
-    data = json.loads(response.read())
+      data = json.loads(response.read())
 
-    if 'id' in data:
-      retval = ''
-      if data['id'] == '117596712775912423303' or data['id'] == '108751342867466333780' or data['id'] == '112336147904981294875' or data['id'] == '104514437420477125478':
-        scripts = [
-          'modules/lowerthird.js',
-          'modules/soundboard.js',
-          'modules/volumecontrol.js',
-          'modules/memeface.js',
-          'modules/commenttracker.js',
-          'hangouttoolbox.js'
-          ]
+      if 'id' in data:
+        retval = ''
+        if data['id'] == '117596712775912423303' or data['id'] == '108751342867466333780' or data['id'] == '112336147904981294875' or data['id'] == '104514437420477125478':
+          scripts = [
+            'modules/lowerthird.js',
+            'modules/soundboard.js',
+            'modules/volumecontrol.js',
+            'modules/memeface.js',
+            'modules/commenttracker.js',
+            'hangouttoolbox.js'
+            ]
+    except:
+      pass
 
-      for script in scripts:
-        file = open(os.path.dirname(__file__) + '/'+ script)
-        retval += '\n' + ''.join(file.readlines())
+    for script in scripts:
+      file = open(os.path.dirname(__file__) + '/'+ script)
+      retval += '\n' + ''.join(file.readlines())
 
-      self.response.write(retval)
-    #except:
-    #  pass
+    self.response.write(retval)
 
 
 app = webapp2.WSGIApplication([
