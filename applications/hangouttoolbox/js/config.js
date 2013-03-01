23cf19881db4bb9a0f8f51e40276e7673ab81455
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2013-03-01 21:42:27 +0000

    Removed donate button

--- applications/hangouttoolbox/js/config.js
+++ applications/hangouttoolbox/js/config.js
@@ -1,4 +1,4 @@
-var version = "1.0.8";
+var version = "1.0.9";
 var faces = [
 	{title : "LOL", scale: 2.4, data : "lolface.png", offset: 0},
 	{title : "BOSS", scale: 2.5, data : "boss.png", offset: 0},
